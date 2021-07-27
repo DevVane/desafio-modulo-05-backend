a108ba3956493a2df6282bf61d13316d835e6d36
Vanessa  Carvalho <80365265+DevVane@users.noreply.github.com>
2021-07-27 20:45:09 -0300

    Cadastro de usuários

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,12 +1,15 @@
 const bcrypt = require('bcrypt');
 const knex = require('../bancodedados/conexao');
-const cadastroUsuarioSquema = require('../validacoes/cadastroUsuarioSquema');
+const { usuarioSquema, restauranteSquema } = require('../validacoes/cadastroUsuarioSquema');
 
 async function cadastrarUsuario(req, res){
-    const { nome, email, senha, restaurante} = req.body;
+    const { nome, email, senha} = req.body;
     
     try {
-        await cadastroUsuarioSquema.validate(req.body);
+        await usuarioSquema.validate(req.body);
+        await restauranteSquema.validate(req.body.restaurante);
+        
+        const { nome: nomeRestaurante, descricao, idCategoria, taxaEntrega, tempoEntregaEmMinutos, valorMinimoPedido} = req.body.restaurante;
 
         const emailEncontrado = await knex('usuario').where({ email }).first();
 
@@ -16,13 +19,30 @@ async function cadastrarUsuario(req, res){
 
         const senhaCriptografada = await bcrypt.hash(senha, 10);
 
-        const usuario = await knex('usuario').insert({ nome, email, senha: senhaCriptografada});
+        const usuario = await knex('usuario').insert({ nome, email, senha: senhaCriptografada}).returning('*');
 
         if (!usuario) {
             return res.status(400).json('Não foi possível cadastrar o usuário');
         }
+        
+        const restauranteCadastrado = await knex('restaurante')
+            .insert({ 
+                usuario_id: usuario[0].id, 
+                nome: nomeRestaurante, 
+                descricao, 
+                categoria_id: idCategoria, 
+                taxa_entrega: taxaEntrega, 
+                tempo_entrega_minutos: tempoEntregaEmMinutos, 
+                valor_minimo_pedido: valorMinimoPedido
+            });
+            
+        if(!restauranteCadastrado) {
+            await knex('usuario').del().where({ id: usuario[0].id });
 
-        return res.status(200).json('Usuário cadastrado com sucesso.');
+            return res.status(400).json('Não foi possível cadastrar o restaurante');
+        }
+
+        return res.status(200).json('Usuário e restaurante cadastrados com sucesso.');
    
     } catch (error) {
         return res.status(400).json(error.message);
--- src/validacoes/cadastroUsuarioSquema.js
+++ src/validacoes/cadastroUsuarioSquema.js
@@ -1,20 +1,21 @@
 const yup = require('./yup');
 
-const cadastroUsuarioSquema = yup.object().shape({
-    nome: yup
-        .string()
-        .required(),
-
-    email: yup
-        .string()
-        .email()
-        .required(),
-
-    senha: yup
-        .string()
-        .required()
-
-    
+const usuarioSquema = yup.object().shape({
+    nome: yup.string().max(100).required(),
+    email: yup.string().max(100).email().required(),
+    senha: yup.string().required()
 })
 
-module.exports = cadastroUsuarioSquema;
+const restauranteSquema = yup.object().shape({
+    nome: yup.string().max(50).required(), 
+    descricao: yup.string().max(100).required(), 
+    idCategoria: yup.number().integer().required(), 
+    taxaEntrega: yup.number().integer().required(), 
+    tempoEntregaEmMinutos: yup.number().integer().required(), 
+    valorMinimoPedido: yup.number().required()   
+})
+
+module.exports = {
+    usuarioSquema,
+    restauranteSquema
+};
